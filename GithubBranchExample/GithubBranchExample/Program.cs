ed0ecb6778144d1f944392dc195cdc0fd11df616
Ahmet Selman Şener <selmansenerr@gmail.com>
2024-09-19 21:37:20 +0300

    Yeni Proje Eklendi

---
+++ GithubBranchExample/GithubBranchExample/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GithubBranchExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
